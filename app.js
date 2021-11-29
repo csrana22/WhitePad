afeef21b32d626b71adcf8204cf0ba6b705d1650
Sameer Rana <srana_be19@thapar.edu>
2021-11-30 01:05:34 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express= require("express"); //access 
+const socket= require("socket.io");
+
+const app= express();  //initialize
+
+
+app.use(express.static("frontend"));
+
+let port=3000;
+
+let server=app.listen(port,()=>{
+    console.log("Listening");
+})
+
+let io=socket(server);
+
+io.on("connection",(socket)=>{
+
+    console.log("New connection");
+
+    socket.on("beginPath",(data)=>{
+
+        io.sockets.emit("beginPath",data);
+    })
+
+    socket.on("drawStroke",(data)=>{
+
+        io.sockets.emit("drawStroke",data);
+    })
+
+    socket.on("redoUndo",(data)=>{
+        io.sockets.emit("redoUndo",data);
+    })
+})
